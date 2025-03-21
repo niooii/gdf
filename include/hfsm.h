6b4375b1687ae0f61aea85651d10f0a9d9ef6758
niooii <onioniwonpounoin@gmail.com>
2025-03-21 17:59:22 -0400

    hiearchical finite state machines are hard

--- include/hfsm.h
+++ include/hfsm.h
@@ -1,12 +1,79 @@
 #pragma once
 
 class State {
+// need a way to store children states. maybe templates will come in clutch
+// or do i even need to store children?
+protected:
+	State* parent;
+
 public:
-    virtual void enter() {}
-    virtual void exit() {}
+	State(State* parent) : parent{parent} {
+		
+	};
+
+    virtual void enter(Entity& e) {}
+    virtual void exit(Entity& e) {}
 
     // User input should be handled here too, probably
-    virtual void update(f32 dt) {}
+    // Return a state to transition, or just return nullptr
+    virtual State* update(Entity& e, f32 dt) {}
 };
 
 // so how do i do this
+
+// example models 
+
+class AirborneState : State {
+	
+}
+
+class JumpingState : AirborneState {
+	
+}
+
+class FallingState : AirborneState {
+	
+}
+
+class GroundedState : State {
+public:
+	void enter(Entity& e) override {
+		
+	}
+
+	void exit(Entity& e) override {
+		
+	}
+
+	State* update(Entity& e, f32 dt) override {
+		if (GDF_IsKeyPressed(GDF_KEYCODE_SPACE)) {
+			// now how do i set the parent state accordingly damn this is hard
+			return new JumpingState();
+		}
+	}
+}
+
+// this could actually belong to an entity as a component - look more into that later	
+class StateMachine {
+	Entity e;
+	State* curr;
+	
+public:
+	StateMachine(State* entry, Entity e) : curr{entry}, e{e} {};
+
+	// also consider an add_transition(std::function<State* transition(State* curr, f32 dt)>) where the fn
+	// returns a new state or nothing. but calling a shit ton of functions doesnt seem that performant
+	// however itll make things so much more clear
+	// however thats basically already the update function man just ff
+	void add_state(State* state);
+
+	// something like this
+	void update(f32 dt) {
+		State* new_state = curr->update(e, dt);
+		if (curr != new_state) {
+			curr->exit(e);
+			curr = new_state;
+			curr->enter(e);
+		}
+	}
+}
