813c9f87246f3aac8154fea7f531cc0239256b55
niooi <onioniwonpounoin@gmail.com>
2025-03-21 20:52:47 -0400

    nothing of substance

--- include/hfsm.h
+++ include/hfsm.h
@@ -1,5 +1,12 @@
 #pragma once
 
 class State {
+public:
+    virtual void enter() {}
+    virtual void exit() {}
 
+    // User input should be handled here too, probably
+    virtual void update(f32 dt) {}
 };
+
+// so how do i do this
